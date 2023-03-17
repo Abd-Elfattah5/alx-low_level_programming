8f1aea5db4782d535c4cd98f0bda09a753b69c56
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-03-17 13:50:27 -0700

    printing my first c sentence

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * this function is made to print a sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+return (0);
+}
