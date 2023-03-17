686deac7f11a147b0040e6c2f26832d6ea210cba
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-03-17 14:10:31 -0700

    using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ *main - Entry point
+ *this is a function to print a sentence using printf function
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
