cb95f9619ebeec37363822f4381fade69a38ecda
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-03-17 19:04:24 -0700

    printing in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *main - funtion to print chars
+ *Return: always 0 (success)
+ */
+int main(void)
+{	int i;
+	for (i = 122; i > 96 ; i--)
+	{
+		char ch;
+
+		ch = i;
+		if (i != 101 && i != 113)
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
