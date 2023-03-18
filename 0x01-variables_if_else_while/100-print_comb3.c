c8a4362783a1c2459dac986625e9ff4976c66b56
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-03-17 19:36:23 -0700

    printing combinations of 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,29 @@
 #include <stdlib.h>
-#;include <time.h>
+#include <time.h>
 #include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
 /**
- *main - funtion to print digits
+ *main - funtion to print digits combinations
  *Return: always 0 (success)
  */
 int main(void)
 {	int i;
+	int j;
 
-	for (i = 48; i <= 57 ; i++)
+	for (i = 0; i < 10 ; i++)
 	{
-		putchar(i);
-		if (i != 57)
+	for (j = i + 1; j < 10; j++)
+	{
+		putchar(i + 48);
+		putchar(j + 48);
+		if (!(i == 8 && j == 9))
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	}
 	putchar('\n');
 	return (0);
