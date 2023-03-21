af5f757f238c9af5bb4254bb4e7ef9c9382a5bc1
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-03-21 16:29:40 -0700

    get the last digit first befor turning into positive

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,11 +5,12 @@
   * @i: the number to be returned
   */
 int print_last_digit(int i)
-{
-	int tmp;
+{	int tmp;
 
-	tmp = _abs(i % 10);
+	tmp = i % 10;
 
+	if (tmp < 0)
+		tmp /= -1;
 	_putchar(tmp + '0');
 	return (tmp);
 }
