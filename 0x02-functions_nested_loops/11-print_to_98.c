2b63e1fd2bbf6020ca0248743c69c633fb579349
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-03-21 18:13:37 -0700

    ptrinting untill the limits

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  *print_to_98 - a function to go to 98
+  *Retuen: always 0 (success)
+  *@n: the number we are going to count to
+  */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else
+				printf("%d\n", i);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else
+				printf("%d\n", j);
+		}
+	}
+	
+}
