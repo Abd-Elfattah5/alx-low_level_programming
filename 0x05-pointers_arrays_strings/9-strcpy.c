a999ab9f7a5f1702f8c5b11a09e546b0788dac16
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-03-27 17:48:25 -0700

    fixed some pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,7 +8,19 @@
   */
 char *_strcpy(char *dest, char *src)
 {
-	*dest = *src;
+	int i = 0;
+	int j = 0;
 
+	while (src[i] != '\0')
+		i++;
+	i--;
+
+	dest[i] = src[i];
+
+	while (j <= i)
+	{
+		dest[j] = src[j];
+		j++;
+	}
 	return (dest);
 }
