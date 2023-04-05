3f3864c92341f1b66bfce645698db530d78806ae
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-04-04 23:44:34 -0700

    finding wether a number is prime or not

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,19 @@
 #include "main.h"
+/**
+  *calc_prime - function to search if the number has any factors
+  *Return: 1 if the number is prime 0 otherwise
+  *@n: the number to be checked
+  *@fac: the factor of the number
+  */
+int calc_prime(int n, int fac)
+{
+	if (fac > n / 2)
+		return (1);
+	else if (n % fac == 0)
+		return (0);
+	else
+		return (calc_prime(n, fac + 1));
+}
 
 /**
   *is_prime_number - funcion to check if a number is prime
@@ -6,5 +21,9 @@
   */
 int is_prime_number(int n)
 {
-
+	if (n < 2)
+		return (0);
+	else
+		return (calc_prime(n, 2));
 }
+
