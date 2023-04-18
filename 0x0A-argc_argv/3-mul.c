0a7340be7e3a3e943653ad4adcecffa8c1c602e0
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-04-18 04:36:22 -0700

    using argc and argv to git number multiplications

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - program to print two numbers multiplication
+  *Return: always (0) success
+  *@argc: number of arguments the program have
+  *@argv: array of pointer to the strings of the arguments
+  */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+		printf("Error");
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
