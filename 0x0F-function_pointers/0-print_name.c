1ba148268537463092029ce5529897cd4909588a
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-04-22 22:57:44 -0700

    followd betty codint style

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,12 @@
 #include "function_pointers.h"
 
 /**
-  *print_name - function that excutes another function as it takes it as parameter
+  *print_name - function that excutes another function takes it as parameter
   *Return: void
   *@name: a pointer to string
   *@f: a pointer to function
   */
 void print_name(char *name, void (*f)(char *))
 {
-	f (name);
-	return;
+	f(name);
 }
