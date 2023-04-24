339b7b1dff6ca042619566b5b5ba956bc035fcb2
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-04-23 23:11:54 -0700

    using variadic functions to print variable number of integers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+  *print_numbers - function to print variable number of integers
+  *Return: always (0) success
+  *@separator: a char to be printed between numbers
+  *@n: number of integers to be printed
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list LP;
+	int temp;
+
+	if (n <= 0)
+		return;
+	va_start(LP, n);
+	for (i = 0; i < n; i++)
+	{
+		temp = va_arg(LP, int);
+
+		printf("%d", temp);
+
+		if (i == n - 1)
+			break;
+		else if (*separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
