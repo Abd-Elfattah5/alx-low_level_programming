8989b981f3162b048f6cc69f4ea426d58ef43365
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-04-27 17:05:36 -0700

    added a function to free linked list space

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  *free_list - function to free the whole space of linked list
+  *Return: void
+  *@head: a pointer to the head of the linked list
+  */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	temp = head;
+
+	while (temp->next)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+	free(head);
+}
