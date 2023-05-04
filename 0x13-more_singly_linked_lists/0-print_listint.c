3b341e2b726a28984d2b486e2c48cb425a79e316
abd el-fattah <abdelafatahwenny@yahoo.com>
2023-05-03 22:11:57 -0700

    followd the requirments

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
   */
 size_t print_listint(const listint_t *h)
 {
-	unsigned int i;
+	size_t i;
 
 	i = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		printf("%d \n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
