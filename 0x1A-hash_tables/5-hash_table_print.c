53054c493649a60b90253d79222efc45195f66d0
abdelfattah <abdelfatahwenny@yahoo.com>
2023-09-08 13:09:16 -0700

    print the hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = (ht->array)[i];
+		while (temp != NULL)
+		{
+			if (flag)
+				printf(", ");
+			printf("\'%s\': \'%s\'", temp->key, temp->value);
+			temp = temp->next;
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
