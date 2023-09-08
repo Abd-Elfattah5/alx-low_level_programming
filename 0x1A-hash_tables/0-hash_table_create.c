9c214f9ae20b2f12d6ddcfe70f29bf49eccb0746
abdelfattah <abdelfatahwenny@yahoo.com>
2023-09-08 10:15:47 -0700

    allocate a pointer space to each array bucket

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	my_table->size = size;
-	my_table->array = (hash_node_t **) malloc(sizeof(hash_node_t) * size);
+	my_table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
 
 	if (my_table->array == NULL)
 	{
